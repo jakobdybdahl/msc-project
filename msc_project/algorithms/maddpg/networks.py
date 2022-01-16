7e4d6cc6f1f037a6bccedb317757905746174d84
Rasmus Østergaard Thorsen <rot@it-minds.dk>
2022-01-16 11:50:29 +0100

    added new version of maddpg

--- msc_project/algorithms/maddpg/networks.py
+++ msc_project/algorithms/maddpg/networks.py
@@ -1,105 +1,29 @@
-import numpy as np
-import torch
+import os
+import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
-
-import os
-
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return -lim, lim
-
-
-class ActorNetwork(nn.Module):
-
-    def __init__(self, observation_size, action_size, name):
-        """
-        :param observation_size: observation size
-        :param action_size: action size
-        """
-        super(ActorNetwork, self).__init__()
-
-        # https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
-        # Dynamic number of hidden neurons to handle for varying observation space.
-        fc1_units = np.floor((observation_size + action_size) / 2).astype(int)
-        fc2_units = np.floor(fc1_units / 2).astype(int)
-
-        self.bn1 = nn.BatchNorm1d(observation_size)
-        self.bn2 = nn.BatchNorm1d(fc1_units)
-        self.bn3 = nn.BatchNorm1d(fc2_units)
-
-        # batch norm has bias included, disable linear layer bias
-        self.fc1 = nn.Linear(observation_size, fc1_units, bias=False)
-        self.fc2 = nn.Linear(fc1_units, fc2_units, bias=False)
-        self.fc3 = nn.Linear(fc2_units, action_size, bias=False)
-
-        self.name = name
-
-        self.reset_parameters()
-
-    def forward(self, observation):
-        """ map a states to action values
-        :param observation: shape == (batch, observation_size)
-        :return: action values
-        """
-
-        x = F.relu(self.fc1(self.bn1(observation)))
-        x = F.relu(self.fc2(self.bn2(x)))
-        x = F.relu(self.fc3(self.bn3(x)))
-        return torch.sigmoid(x)
-
-
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-
-    def save_checkpoint(self, path):
-        file = os.path.join(path, self.name)
-        torch.save(self.state_dict(), file)
-
-    def load_checkpoint(self, path):
-        file = os.path.join(path, self.name)
-        self.load_state_dict(torch.load(file))
-
-    def save_best(self, path):
-        file = os.path.join(path, self.name + "_best")
-        torch.save(self.state_dict(), file)
-
+import torch.optim as optim
+import numpy as np
 
 class CriticNetwork(nn.Module):
-
-    def __init__(self, observation_size, action_size, name):
-        """
-        :param observation_size: Dimension of each state
-        :param action_size: Dimension of each state
-        """
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, 
+                    n_agents, n_actions, name):
         super(CriticNetwork, self).__init__()
-
-        # https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
-        # Dynamic number of hidden neurons to handle for varying observation space.
-        self.fc1_dims = np.floor((observation_size + action_size) / 2).astype(int)
-        self.fc2_dims = np.floor(self.fc1_dims / 2).astype(int)
-
-        # self.bn1 = nn.BatchNorm1d(observation_size + action_size)
-        # self.bn2 = nn.BatchNorm1d(fc1_units)
-
-        # self.fc1 = nn.Linear(observation_size + action_size, fc1_units, bias=False)
-        # self.fc2 = nn.Linear(fc1_units, fc2_units)
-        # self.fc3 = nn.Linear(fc2_units, 1)
 
         self.name = name
 
-        self.fc1 = nn.Linear(observation_size, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc1 = nn.Linear(input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.q = nn.Linear(fc2_dims, 1)
+        self.action_value = nn.Linear(n_agents*n_actions, fc2_dims)
 
-        self.bn1 = nn.LayerNorm(self.fc1_dims)
-        self.bn2 = nn.LayerNorm(self.fc2_dims)
+        self.bn1 = nn.LayerNorm(fc1_dims)
+        self.bn2 = nn.LayerNorm(fc2_dims)
 
-        self.action_value = nn.Linear(action_size, self.fc2_dims)
-
-        self.q = nn.Linear(self.fc2_dims, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+ 
+        self.to(self.device)
 
         f1 = 1.0 / np.sqrt(self.fc1.weight.data.size()[0])
         self.fc1.weight.data.uniform_(-f1, f1)
@@ -117,16 +41,13 @@ class CriticNetwork(nn.Module):
         self.action_value.weight.data.uniform_(-f4, f4)
         self.action_value.bias.data.uniform_(-f4, f4)
 
-        #self.reset_parameters()
-
-    # def forward(self, observation, action):
-    #     x = torch.cat([observation, action], dim=1)
-    #     x = F.relu(self.fc1(self.bn1(x)))
-    #     x = F.relu(self.fc2(self.bn2(x)))
-    #     x = self.fc3(x)
-    #     return x
-    
     def forward(self, state, action):
+        # x = F.relu(self.fc1(T.cat([state, action], dim=1)))
+        # x = F.relu(self.fc2(x))
+        # q = self.q(x)
+
+        # return q
+
         state_value = self.fc1(state)
         state_value = self.bn1(state_value)
         state_value = F.relu(state_value)
@@ -134,25 +55,77 @@ class CriticNetwork(nn.Module):
         state_value = self.bn2(state_value)
 
         action_value = self.action_value(action)
-        state_action_value = F.relu(torch.add(state_value, action_value))
+        state_action_value = F.relu(T.add(state_value, action_value))
 
         state_action_value = self.q(state_action_value)
 
         return state_action_value
 
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-
     def save_checkpoint(self, path):
         file = os.path.join(path, self.name)
-        torch.save(self.state_dict(), file)
+        T.save(self.state_dict(), file)
 
     def load_checkpoint(self, path):
         file = os.path.join(path, self.name)
-        self.load_state_dict(torch.load(file))
+        self.load_state_dict(T.load(file))
 
     def save_best(self, path):
         file = os.path.join(path, self.name + "_best")
-        torch.save(self.state_dict(), file)
+        T.save(self.state_dict(), file)
+
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, 
+                 n_actions, name):
+        super(ActorNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+
+        self.bn1 = nn.LayerNorm(fc1_dims)
+        self.bn2 = nn.LayerNorm(fc2_dims)
+
+        self.mu = nn.Linear(fc2_dims, n_actions)
+
+        f2 = 1.0 / np.sqrt(self.fc2.weight.data.size()[0])
+        self.fc2.weight.data.uniform_(-f2, f2)
+        self.fc2.bias.data.uniform_(-f2, f2)
+
+        f1 = 1.0 / np.sqrt(self.fc1.weight.data.size()[0])
+        self.fc1.weight.data.uniform_(-f1, f1)
+        self.fc1.bias.data.uniform_(-f1, f1)
+
+        f3 = 0.003
+        self.mu.weight.data.uniform_(-f3, f3)
+        self.mu.bias.data.uniform_(-f3, f3)
+
+        self.name = name
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+ 
+        self.to(self.device)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = T.sigmoid(self.mu(x))
+
+        return x
+
+    def save_checkpoint(self, path):
+        file = os.path.join(path, self.name)
+        T.save(self.state_dict(), file)
+
+    def load_checkpoint(self, path):
+        file = os.path.join(path, self.name)
+        self.load_state_dict(T.load(file))
+
+    def save_best(self, path):
+        file = os.path.join(path, self.name + "_best")
+        T.save(self.state_dict(), file)
+
