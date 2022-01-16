7125c678adebb8052c335f405bfe7c4cfc932c64
Rasmus Østergaard Thorsen <rot@it-minds.dk>
2022-01-16 13:03:00 +0100

    Changed to shared agent for MADDPG

--- msc_project/algorithms/maddpg/maddpg_agent.py
+++ msc_project/algorithms/maddpg/maddpg_agent.py
@@ -39,22 +39,22 @@ class MADDPGAgent:
             mean=0, std=args.act_noise_std_start, decay=decay, min_std=args.act_noise_std_min
         )
 
-        for agent_idx in range(self.n_agents):
-            self.agents.append(Agent(self.obs_dim, self.obs_dim * self.n_agents,  
-                            self.act_dims, self.n_agents, agent_idx, self.noise, decay, fc1=self.fc1_dims, fc2=self.fc2_dims))
-        # self.agent = Agent(self.obs_dim, self.obs_dim * self.n_agents,  
-        #                     self.act_dims, self.n_agents, 0, self.noise, decay, fc1=self.fc1_dims, fc2=self.fc2_dims)
+        # for agent_idx in range(self.n_agents):
+        #     self.agents.append(Agent(self.obs_dim, self.obs_dim * self.n_agents,  
+        #                     self.act_dims, self.n_agents, agent_idx, self.noise, decay, fc1=self.fc1_dims, fc2=self.fc2_dims))
+        self.agent = Agent(self.obs_dim, self.obs_dim * self.n_agents,  
+                            self.act_dims, self.n_agents, 0, self.noise, decay, fc1=self.fc1_dims, fc2=self.fc2_dims)
 
 
     def save_models(self, path):
         print('... saving model ...')
-        for agent in self.agents:
-            agent.save_models(path)
+        # for agent in self.agents:
+        self.agent.save_models(path)
 
     def load_models(self, path):
         print('... loading model ...')
-        for agent in self.agents:
-            agent.load_models(path)
+        # for agent in self.agents:
+        self.agent.load_models(path)
 
     def get_random_actions(self):
         return [self.act_space.sample()[0] for _ in range(self.n_agents)]
@@ -62,8 +62,8 @@ class MADDPGAgent:
     def get_actions(self, raw_obs, explore=True):
         raw_obs = raw_obs["fov"]
         actions = []
-        for agent_idx, agent in enumerate(self.agents):
-            action = agent.choose_action(raw_obs[agent_idx], explore)
+        for agent_idx in range(self.n_agents):
+            action = self.agent.choose_action(raw_obs[agent_idx], explore)
             actions.append(action)
         return actions
 
@@ -92,16 +92,16 @@ class MADDPGAgent:
         all_agents_new_mu_actions = []
         old_agents_actions = []
 
-        for agent_idx, agent in enumerate(self.agents):
+        for agent_idx in range(self.n_agents):
             new_states = T.tensor(actor_new_states[agent_idx], 
                                  dtype=T.float).to(self.device)
 
-            new_pi = agent.target_actor.forward(new_states)
+            new_pi = self.agent.target_actor.forward(new_states)
 
             all_agents_new_actions.append(new_pi)
             mu_states = T.tensor(actor_states[agent_idx], 
                                  dtype=T.float).to(self.device)
-            pi = agent.actor.forward(mu_states)
+            pi = self.agent.actor.forward(mu_states)
             all_agents_new_mu_actions.append(pi)
             old_agents_actions.append(actions[agent_idx])
 
@@ -109,30 +109,30 @@ class MADDPGAgent:
         mu = T.cat([acts for acts in all_agents_new_mu_actions], dim=1)
         old_actions = T.cat([acts for acts in old_agents_actions],dim=1)
 
-        for agent_idx, agent in enumerate(self.agents):
-            agent.actor.optimizer.zero_grad()
-        # self.agent.actor.optimizer.zero_grad()
+        # for agent_idx, agent in enumerate(self.agents):
+        #     agent.actor.optimizer.zero_grad()
+        self.agent.actor.optimizer.zero_grad()
         
-        for agent_idx, agent in enumerate(self.agents):
-            critic_value_ = agent.target_critic.forward(states_, new_actions).flatten()
+        for agent_idx in range(self.n_agents):
+            critic_value_ = self.agent.target_critic.forward(states_, new_actions).flatten()
             critic_value_[dones[:,0]] = 0.0
-            critic_value = agent.critic.forward(states, old_actions).flatten()
+            critic_value = self.agent.critic.forward(states, old_actions).flatten()
 
-            target = rewards[:,agent_idx] + agent.gamma*critic_value_
+            target = rewards[:,agent_idx] + self.agent.gamma*critic_value_
             critic_loss = F.mse_loss(target, critic_value)
-            agent.critic.optimizer.zero_grad()
+            self.agent.critic.optimizer.zero_grad()
             critic_loss.backward(retain_graph=True)
-            agent.critic.optimizer.step()
+            self.agent.critic.optimizer.step()
 
-            actor_loss = agent.critic.forward(states, mu).flatten()
+            actor_loss = self.agent.critic.forward(states, mu).flatten()
             actor_loss = -T.mean(actor_loss)
             actor_loss.backward(retain_graph=True)
             
-        for agent_idx, agent in enumerate(self.agents):
-            agent.actor.optimizer.step()
-            agent.update_network_parameters()
-        # self.agent.actor.optimizer.step()
-        # self.agent.update_network_parameters()
+        # for agent_idx, agent in enumerate(self.agents):
+        #     agent.actor.optimizer.step()
+        #     agent.update_network_parameters()
+        self.agent.actor.optimizer.step()
+        self.agent.update_network_parameters()
 
     def _obs_list_to_state_vector(self, observation):
         state = np.array([])
